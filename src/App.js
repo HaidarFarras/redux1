1c5df5ec6f14317598b62022af70a14c6d8e2cf2
Haidar Farras <haidarlatief0@gmail.com>
2019-01-27 13:23:03 +0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,52 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { connect } from 'react-redux';
+import Notes from './Notes';
 
 class App extends Component {
+state = { count: 0 }
+increment = () => {
+  this.props.dispatch({type:'INCREMENT'});
+}
+decrement = () => {
+  this.props.dispatch({type:'DECREMENT'});
+}
+incrementDua = () => {
+  this.props.dispatch({type:'INCREMENTDUA', plus: 2});
+}
+decrementDua = () => {
+  this.props.dispatch({type:'DECREMENTDUA', minus: 2});
+}
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div>
+          <Notes/>
+          <button onClick = {() => this.decrement()}>
+            Kurang
+          </button>
+          <span></span>
+          <button onClick = {() => this.increment()}>
+            Tambah
+          </button>
+          <span></span>
+          <button onClick = {() => this.incrementDua()}>
+          Tambah + 2
+          </button>
+          <span/>
+          <button onClick = {() => this.decrementDua()}>
+          Kurang - 2
+          </button>
+        </div>
       </div>
     );
   }
 }
+function mapStateToProps(state){
+  return {
+    count: state.count
+  };
+}
 
-export default App;
+export default connect(mapStateToProps)(App);
